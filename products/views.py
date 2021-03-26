a95a71989e23807858a99f0327fb6f21ad1d5ec8
christian <christian.spitler@cinteot.com>
2021-03-26 07:21:16 -0400

    trying to resolve untracked files

--- products/views.py
+++ products/views.py
@@ -1,17 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def home_view(request, *args, **kwargs):
- #return HttpResponse("<h1>Hello World</h1>") #string of HTML code, not actually HTML
-    return render(request, "home.html", {})
-def contact_view(request, *args, **kwargs):
-    #return HttpResponse("<h1>Contact Page</h1>")
-    return render(request, "contact.html", {})
-def about_view(request, *args, **kwargs):
-    my_context = {
-        "my_text": "This is about us",
-        "this_is_true": True,
-        "my_number": 123,
-        "my_list": [122,23,45,99, "Abc"],
+from .models import Product
+
+def product_detail_view(request,*args,**kwargs):
+    obj = Product.objects.get(id=1)
+    context = {
+        'title': obj.title
+        'description': obj.description
     }
-    return render(request, "about.html", my_context)
+    return render(request, "product/detail.html", context)
